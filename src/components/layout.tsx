0c04ec5c63215a85f1ec0bf1f1c480c109eb544c
Adrien Baudet <baudetad@gmail.com>
2023-10-09 19:32:26 +0200

    update margins

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -43,7 +43,7 @@ const Layout = ({ pageTitle, children }: Props) => {
                     </Link>
                 </div>
             </Menu>
-            <main className='p-4  md:mx-96 bg-indigo-700'>
+            <main className='p-4  md:mx-60 xl:mx-80 bg-indigo-700'>
                 <h1 className='text-center'>{pageTitle}</h1>
                 {children}
             </main>
